05f5c6ab2cd245e347a2ee9a42b3bb57a2329833
alberto19963-rgb <alberto19963@gmail.com>
2026-04-23 12:34:09 -0400

    mejora de capacidad de envio

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use('/', routes);
 
